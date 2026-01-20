83bd1a5e8e7fa95126f49850f19a419ca9c57252
JokkeIver <jc.ivers1@gmail.com>
2026-01-21 00:05:50 +0100

    [BEGIN WORK] Began working on the tracking of throws

--- src/routes/matches/+page.svelte
+++ src/routes/matches/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-	import { forEachTrailingCommentRange } from "typescript";
+let player1 = "John Doe";
+let player2 = "Jane Doe";
 
-  let player1 = "John Doe";
-  let player2 = "Jane Doe";
+let bestOfNum = 3;
+let legs = [0, 0];
 
-  let bestOfNum = 3;
-  let legs = [0, 0];
+let player1left = 501;
+let player2left = 501;
 
-  function toggleTurn() {
-    let players = document.getElementsByClassName("playerName");
-    players[0].classList.toggle("turn");
-    players[1].classList.toggle("turn");
-  }
+
+function toggleTurn() {
+  let players = document.getElementsByClassName("playerName");
+  players[0].classList.toggle("turn");
+  players[1].classList.toggle("turn");
+}
 </script>
 
 <div class="header">
@@ -36,7 +38,50 @@
   <div class="legScoreContainer">
     <p>Best of {bestOfNum}</p>
     <p class="legScore">{legs[0]} : {legs[1]}</p>
+
+    <div class="roundScoreContainer">
+      <div class="playerLeft">
+        <label>Remaining:</label>
+        <p class="RoundScore">{player1left}</p>
+      </div>
+      <div class="playerLeft">
+        <label>Remaining:</label>
+        <p class="RoundScore">{player2left}</p>
+      </div>
+    </div>
   </div>
+
+  <!-- Input number pad -->
+  <div class="inputContainer">
+    <div class="multipliers">
+      <button class="double">Double</button>
+      <button class="triple">Triple</button>
+    </div>
+    <div class="throwInput">
+      <button class="throwNum" value="1">1</button> 
+      <button class="throwNum" value="2">2</button> 
+      <button class="throwNum" value="3">3</button> 
+      <button class="throwNum" value="4">4</button> 
+      <button class="throwNum" value="5">5</button> 
+      <button class="throwNum" value="6">6</button> 
+      <button class="throwNum" value="7">7</button> 
+      <button class="throwNum" value="8">8</button> 
+      <button class="throwNum" value="9">9</button> 
+      <button class="throwNum" value="10">10</button> 
+      <button class="throwNum" value="11">11</button> 
+      <button class="throwNum" value="12">12</button> 
+      <button class="throwNum" value="13">13</button> 
+      <button class="throwNum" value="14">14</button> 
+      <button class="throwNum" value="15">15</button> 
+      <button class="throwNum" value="16">16</button> 
+      <button class="throwNum" value="17">17</button> 
+      <button class="throwNum" value="18">18</button> 
+      <button class="throwNum" value="19">19</button> 
+      <button class="throwNum" value="20">20</button> 
+      <button class="throwNum" value="25">25</button> 
+    </div>
+  </div>
+
 </div>
 
 <style>
@@ -105,6 +150,8 @@
 
 /* Handle the container for standing in legs */
 .legScoreContainer {
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
@@ -115,5 +162,19 @@
 
 .legScoreContainer > p:last-child {
   margin-top: 3px;
+}
+
+/* Handle the container for the current round score */
+.roundScoreContainer {
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+}
+
+.playerLeft {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
